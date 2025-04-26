1493b1d9ece1463acb3b9c45fe3663e2bb9bd1a5
Reveroftrillion <reverofmillion@gmail.com>
2025-04-26 19:42:42 +0900

    Solve 3009 (py) [Bronze III]

--- BOJ/3009/Main.py
+++ BOJ/3009/Main.py
@@ -1,1 +1,13 @@
-// 3009 네 번째 점 [3]
+x = list()
+y = list()
+for i in range(3):
+    a, b = map(int, input().split())
+    if(x.count(a) == 0):
+        x.append(a)
+    else: 
+        x.remove(a)
+    if(y.count(b) == 0):
+        y.append(b),
+    else:
+        y.remove(b)
+print(x[0], y[0])
