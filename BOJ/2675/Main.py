3713b52e1996f58dfda31102bedf2a73775bce48
Reveroftrillion <reverofmillion@gmail.com>
2025-04-26 19:25:51 +0900

    Solve 2675 (py) [Bronze II]

---
+++ BOJ/2675/Main.py
@@ -0,0 +1,10 @@
+# 2675 문자열 반복 [Bronze II]
+import sys
+
+def main():
+    data = sys.stdin.read().split()
+    # 여기에 문제 풀이 코드를 작성하세요
+    # 예시: print(sum(map(int, data)))
+
+if __name__ == "__main__":
+    main()
