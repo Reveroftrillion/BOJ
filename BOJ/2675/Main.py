716873471044d6a94a7dc6585fee2e2b12240145
Reveroftrillion <reverofmillion@gmail.com>
2025-04-26 19:42:53 +0900

    Solve 2675 (py) [Bronze II]

--- BOJ/2675/Main.py
+++ BOJ/2675/Main.py
@@ -1,10 +1,7 @@
-# 2675 문자열 반복 [Bronze II]
-import sys
-
-def main():
-    data = sys.stdin.read().split()
-    # 여기에 문제 풀이 코드를 작성하세요
-    # 예시: print(sum(map(int, data)))
-
-if __name__ == "__main__":
-    main()
+A = int(input())
+for i in range(A):
+    B, C = input().split()
+    D = int(B)
+    for char in C:
+        print(char*D, end='')
+    print()
