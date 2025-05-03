a2aabfc071063a27af0e2e39f159164cb8002d46
Reveroftrillion <reverofmillion@gmail.com>
2025-05-03 22:51:42 +0900

    Solve 32154 (cpp) [Bronze V]

---
+++ BOJ/32154/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    if (t == 1)
+    {
+        cout << "11" << endl;
+        cout << "A B C D E F G H J L M" << endl;
+    }
+    else if (t == 2)
+    {
+        cout << "9" << endl;
+        cout << "A C E F G H I L M" << endl;
+    }
+    else if (t == 3)
+    {
+        cout << "9" << endl;
+        cout << "A C E F G H I L M" << endl;
+    }
+    else if (t == 4)
+    {
+        cout << "9" << endl;
+        cout << "A B C E F G H L M" << endl;
+    }
+    else if (t == 10)
+    {
+        cout << "8" << endl;
+        cout << "A B C F G H L M" << endl;
+    }
+    else
+    {
+        cout << "8" << endl;
+        cout << "A C E F G H L M" << endl;
+    }
+}
