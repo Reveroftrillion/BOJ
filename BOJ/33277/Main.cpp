b669ee121b9686833ac40ac52a1bf480e380a16b
Reveroftrillion <reverofmillion@gmail.com>
2025-05-14 23:11:23 +0900

    Solve 33277 (cpp) [Bronze III]

--- BOJ/33277/Main.cpp
+++ BOJ/33277/Main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     int a, b;
     cin >> a >> b;
-    double percent = (double)b / a;
-    int time = percent * 24 * 60;
+    int time = b * 1440 / a;
     int hour = time / 60;
     int minute = time % 60;
-    cout << hour << ":" << minute << endl;
+    cout << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minute << endl;
 }
