666ed6b2fe10d8552147b252a519ed7d551ba828
Reveroftrillion <reverofmillion@gmail.com>
2025-06-05 00:58:56 +0900

    Solve 32246 (cpp) [Bronze III]

---
+++ BOJ/32246/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    if (t == 2)
+        cout << 3 << '\n';
+    else
+        cout << t << '\n';
+    return 0;
+}
