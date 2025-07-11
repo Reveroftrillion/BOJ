1f080104a5c637d2095f3db97fd343c98bbe740e
Reveroftrillion <reverofmillion@gmail.com>
2025-07-11 22:49:40 +0900

    Solve 15489 (cpp) [Silver IV]

---
+++ BOJ/15489/Main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r, c, w, pas[40][40], ans;
+
+void makePascal()
+{
+    for (int i = 1; i <= 31; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i == 1 || j == 1)
+                pas[i][j] = 1;
+            else
+                pas[i][j] = pas[i - 1][j] + pas[i - 1][j - 1];
+        }
+    }
+}
+
+int main()
+{
+    cin >> r >> c >> w;
+    makePascal();
+
+    for (int i = r; i < r + w; i++)
+    {
+        for (int j = c; j <= c + i - r; j++)
+        {
+            ans += pas[i][j];
+        }
+    }
+
+    cout << ans;
+}
