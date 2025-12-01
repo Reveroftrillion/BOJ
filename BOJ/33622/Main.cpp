7c0a78a2f7d6c484cd0b934ef258412683e5abf6
Reveroftrillion <reverofmillion@gmail.com>
2025-12-01 15:23:48 +0900

    Solve 33622 (cpp) [Silver V]

---
+++ BOJ/33622/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long N;
+    if (!(cin >> N))
+        return 0;
+    cout << 1 << '\n'
+         << N << '\n';
+    return 0;
+}
